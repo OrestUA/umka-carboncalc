890e7dc485d3eac70ee0dfb2c9e05adc679b157b
Basil Dudka <dudka.vasia2010@gmail.com>
2016-12-03 16:28:42 +0200

    first version

--- js/vd-script.js
+++ js/vd-script.js
@@ -1,5 +1,18 @@
 $(function(){
-    $(".pbarItemCurrent").each(function() {
-      $(this).css("border-color", "green");
-    })
+    var currentQuestion=1;
+    var marHeight=$('.question#q'+currentQuestion).height();
+    $('.questionOption').css('margin-top',marHeight+'px');
+    $('.questionNum').text('Питання '+currentQuestion);
+    $('.questionButton').click(function() {
+      currentQuestion+=1;
+      $(".pbarItem#pb"+currentQuestion).addClass('pbarItemCurrent');
+      $('.question').each(function() {
+        $(this).css('visibility','hidden');
+      });
+      $('.question#q'+currentQuestion).css('visibility','visible');
+      marHeight=$('.question#q'+currentQuestion).height();
+      $('.questionOption').css('margin-top',marHeight+'px');
+      $('.questionNum').text('Питання '+currentQuestion);
+    });
+
 });
