48322fb7b5f56fbb00afbcc0119bd3318ead6764
Basil Dudka <dudka.vasia2010@gmail.com>
2016-12-03 22:46:46 +0200

    little fixes

--- js/vd-script.js
+++ js/vd-script.js
@@ -7,54 +7,56 @@ $(function(){
     $('.questionOption').css('margin-top', marginGroupHeight+'px');
 
     var marginItemHeight=$('.questionOptionVariant#qov'+currentQuestion).height()+10;
-    $('.questionButton').css('margin-top', marginItemHeight+'px');
+    $('#questionButton').css('margin-top', marginItemHeight+'px');
+    $('#skipButton').css('margin-top', marginItemHeight+'px');
 
     $('.questionNum').text('Питання '+currentQuestion);
 
     var firstInput=$('.questionOptionVariant#qov'+currentQuestion).children().first().val();
     var lastInput=$('.questionOptionVariant#qov'+currentQuestion).children().last().val();
 
-    $('.questionButton').click(function() {
+    $('#questionButton, #skipButton').click(function() {
           $(".pbarItem#pb"+currentQuestion).removeClass('pbarItemCurrent');
           $(".pbarItem#pb"+currentQuestion).addClass('pbarItemVisited');
 
           firstInput=$('.questionOptionVariant#qov'+currentQuestion).children().first().val();
           lastInput=$('.questionOptionVariant#qov'+currentQuestion).children().last().val();
-          switch(currentQuestion) {
-            case 1:
-            user['rooms']=firstInput;
-            break;
-            case 2:
-            user['roommates']=firstInput;
-            break;
-            case 3:
-            user['electricity']=firstInput;
-            break;
-            case 4:
-            user['gas']=firstInput;
-            break;
-            case 5:
-            user['hotWater']=firstInput;
-            break;
-            case 6:
-            user['carEngineVolume']=firstInput;
-            user['carDistance']=lastInput;
-            break;
-            case 7:
-            user['airTravel']=firstInput;
-            break;
-            case 8:
-            user['age']=firstInput;
-            break;
-            case 9:
-            user['gender']=$('input:checked').val();
-            break;
-            case 10:
-            user['salary']=firstInput;
-            break;
-            default:
-            break;
-          }
+          if ($(this).attr('id')=='questionButton')
+            switch(currentQuestion) {
+              case 1:
+              user['rooms']=firstInput;
+              break;
+              case 2:
+              user['roommates']=firstInput;
+              break;
+              case 3:
+              user['electricity']=firstInput;
+              break;
+              case 4:
+              user['gas']=firstInput;
+              break;
+              case 5:
+              user['hotWater']=firstInput;
+              break;
+              case 6:
+              user['carEngineVolume']=firstInput;
+              user['carDistance']=lastInput;
+              break;
+              case 7:
+              user['airTravel']=firstInput;
+              break;
+              case 8:
+              user['age']=firstInput;
+              break;
+              case 9:
+              user['gender']=$('input.gender:checked').val();
+              break;
+              case 10:
+              user['salary']=$('input.salary:checked').val();
+              break;
+              default:
+              break;
+            }
 
           currentQuestion+=1;
           $(".pbarItem#pb"+currentQuestion).addClass('pbarItemCurrent');
@@ -69,7 +71,8 @@ $(function(){
           marginGroupHeight=$('.question#q'+currentQuestion).height();
           marginItemHeight=$('.questionOptionVariant#qov'+currentQuestion).height()+10;
           $('.questionOption').css('margin-top', marginGroupHeight+'px');
-          $('.questionButton').css('margin-top', marginItemHeight+'px');
+          $('#questionButton').css('margin-top', marginItemHeight+'px');
+          $('#skipButton').css('margin-top', marginItemHeight+'px');
           $('.questionNum').text('Питання '+currentQuestion);
     });
 
