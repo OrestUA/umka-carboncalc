664a0f5aa94d20b4135d073744d0d220bb90396c
Basil Dudka <dudka.vasia2010@gmail.com>
2016-12-03 17:19:42 +0200

    questions set

--- js/vd-script.js
+++ js/vd-script.js
@@ -4,6 +4,8 @@ $(function(){
     $('.questionOption').css('margin-top',marHeight+'px');
     $('.questionNum').text('Питання '+currentQuestion);
     $('.questionButton').click(function() {
+      $(".pbarItem#pb"+currentQuestion).removeClass('pbarItemCurrent');
+      $(".pbarItem#pb"+currentQuestion).addClass('pbarItemVisited');
       currentQuestion+=1;
       $(".pbarItem#pb"+currentQuestion).addClass('pbarItemCurrent');
       $('.question').each(function() {
