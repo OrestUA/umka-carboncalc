6fe1f5a8b65ce1060dc77186ce0be0bab99cb6d9
graniron <yavorco1991@gmail.com>
2016-12-04 13:34:05 +0200

    Bar and Pie chart

--- js/script.js
+++ js/script.js
@@ -27,31 +27,31 @@ $(document).ready(function() {
 		});
 		
 		//Wow Animation DISABLE FOR ANIMATION MOBILE/TABLET
-		wow = new WOW(
-		{
-			mobile: false
-		});
-		wow.init();
+		// wow = new WOW(
+		// {
+		// 	mobile: false
+		// });
+		// wow.init();
 		
-		//MagnificPopup
-		$('.image-link').magnificPopup({type:'image'});
+		// //MagnificPopup
+		// $('.image-link').magnificPopup({type:'image'});
 
 
-		// OwlCarousel N1
-		$("#owl-demo").owlCarousel({
-			autoPlay: 3000,
-			items : 3,
-			itemsDesktop : [1199,3],
-			itemsDesktopSmall : [979,3]
-		});
+		// // OwlCarousel N1
+		// $("#owl-demo").owlCarousel({
+		// 	autoPlay: 3000,
+		// 	items : 3,
+		// 	itemsDesktop : [1199,3],
+		// 	itemsDesktopSmall : [979,3]
+		// });
 
-		// OwlCarousel N2
-		$("#owl-demo-1").owlCarousel({
-			  navigation : false, // Show next and prev buttons
-			  slideSpeed : 300,
-			  paginationSpeed : 400,
-			  singleItem:true
-		});
+		// // OwlCarousel N2
+		// $("#owl-demo-1").owlCarousel({
+		// 	  navigation : false, // Show next and prev buttons
+		// 	  slideSpeed : 300,
+		// 	  paginationSpeed : 400,
+		// 	  singleItem:true
+		// });
 
 		//SmothScroll
 		$('a[href*=#]').click(function() {
@@ -77,11 +77,14 @@ $(document).ready(function() {
 			console.log(window.location); 
 		});
 		
-		//Subscribe
-		new UIMorphingButton( document.querySelector( '.morph-button' ) );
-		// for demo purposes only
-		[].slice.call( document.querySelectorAll( 'form button' ) ).forEach( function( bttn ) { 
-			bttn.addEventListener( 'click', function( ev ) { ev.preventDefault(); } );
-		} );
+		//Subscribe		
+		if (typeof UIMorphingButton !== 'undefined') {
+			new UIMorphingButton( document.querySelector( '.morph-button' ) );
+			// for demo purposes only
+			[].slice.call( document.querySelectorAll( 'form button' ) ).forEach( function( bttn ) { 
+				bttn.addEventListener( 'click', function( ev ) { ev.preventDefault(); } );
+			} );
+		}
+	
 });
 
