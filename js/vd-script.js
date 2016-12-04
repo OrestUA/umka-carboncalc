c62d3cd420cdedbb0927ba273cb5df30c29bfca6
graniron <yavorco1991@gmail.com>
2016-12-04 10:23:47 +0200

    Handle calculator data sending

--- js/vd-script.js
+++ js/vd-script.js
@@ -24,108 +24,48 @@ $(function(){
     var firstInput=$('.questionOptionVariant#qov'+currentQuestion).children().first().val();
     var lastInput=$('.questionOptionVariant#qov'+currentQuestion).children().last().val();
 
-    /*$('#questionButton, #skipButton').click(function() {
-          $(".pbarItem#pb"+currentQuestion).removeClass('pbarItemCurrent');
-          $(".pbarItem#pb"+currentQuestion).addClass('pbarItemVisited');
-
-          firstInput=$('.questionOptionVariant#qov'+currentQuestion).children().first().val();
-          lastInput=$('.questionOptionVariant#qov'+currentQuestion).children().last().val();
-          if ($(this).attr('id')=='questionButton')
-            switch(currentQuestion) {
-              case 1:
-              user['rooms']=firstInput;
-              break;
-              case 2:
-              user['roommates']=firstInput;
-              break;
-              case 3:
-              user['electricity']=firstInput;
-              break;
-              case 4:
-              user['gas']=firstInput;
-              break;
-              case 5:
-              user['hotWater']=firstInput;
-              break;
-              case 6:
-              user['carEngineVolume']=firstInput;
-              user['carDistance']=lastInput;
-              break;
-              case 7:
-              user['airTravel']=firstInput;
-              break;
-              case 8:
-              user['age']=firstInput;
-              break;
-              case 9:
-              user['gender']=$('input.gender:checked').val();
-              break;
-              case 10:
-              user['salary']=$('input.salary:checked').val();
-              break;
-              default:
-              break;
-            }
-
-          currentQuestion+=1;
-          $(".pbarItem#pb"+currentQuestion).addClass('pbarItemCurrent');
-          $('.question').each(function() {
-            $(this).css('visibility','hidden');
-          });
-          $('.questionOptionVariant').each(function() {
-            $(this).css('visibility','hidden');
-          });
-          $('.question#q'+currentQuestion).css('visibility','visible');
-          $('.questionOptionVariant#qov'+currentQuestion).css('visibility','visible');
-          marginGroupHeight=$('.question#q'+currentQuestion).height();
-          marginItemHeight=$('.questionOptionVariant#qov'+currentQuestion).height()+10;
-          $('.questionOption').css('margin-top', marginGroupHeight+'px');
-          $('#questionButton').css('margin-top', marginItemHeight+'px');
-          $('#skipButton').css('margin-top', marginItemHeight+'px');
-          $('.questionNum').text('Питання '+currentQuestion);
-    });
-    */
-    $('#questionButton, #skipButton').click(function() {
+    $('#questionButton').click(function() {
           $("li#pb"+currentQuestion).removeClass('is-active');
           $("li#pb"+currentQuestion).addClass('is-complete');
 
           firstInput=$('.questionOptionVariant#qov'+currentQuestion).children().first().val();
           lastInput=$('.questionOptionVariant#qov'+currentQuestion).children().last().val();
-          if ($(this).attr('id')=='questionButton')
+          
             switch(currentQuestion) {
               case 1:
-              user['rooms']=firstInput;
-              break;
+                user['rooms']=firstInput;
+                break;
               case 2:
-              user['roommates']=firstInput;
-              break;
+                user['roommates']=firstInput;
+                break;
               case 3:
-              user['electricity']=firstInput;
-              break;
+                user['electricity']=firstInput;
+                break;
               case 4:
-              user['gas']=firstInput;
-              break;
+                user['gas']=firstInput;
+                break;
               case 5:
-              user['hotWater']=firstInput;
-              break;
+                user['hotWater']=firstInput;
+                break;
               case 6:
-              user['carEngineVolume']=firstInput;
-              user['carDistance']=lastInput;
-              break;
+                user['carEngineVolume']=firstInput;
+                user['carDistance']=lastInput;
+                break;
               case 7:
-              user['airTravel']=firstInput;
-              break;
+                user['airTravel']=firstInput;
+                break;
               case 8:
-              user['age']=firstInput;
-              break;
+                user['age']=firstInput;
+                break;
               case 9:
-              user['gender']=$('input.gender:checked').val();
-              break;
+                user['gender']=$('input.gender:checked').val();
+                break;
               case 10:
-              user['salary']=$('input.salary:checked').val();
-              break;
+                user['salary']=$('input.salary:checked').val();
+                sendFormData();
+                break;
               default:
-              break;
+                break;
             }
 
           currentQuestion+=1;
@@ -148,4 +88,17 @@ $(function(){
           mobileCurrentQuestion=10*(currentQuestion-1);
           $('#mobileProgressBar').css('width',mobileCurrentQuestion+'%');
     });
+
+    function sendFormData() {
+      // console.log(11, user);
+      $.post( "http://172.23.76.243:8080/carboncalc", user, function(data) {
+        console.log(data);
+        // Put the object into storage
+        localStorage.setItem('resultData', JSON.stringify(data));
+        window.location.href = 'results.html';
+      })
+      .fail(function() {
+        console.error( "error" );
+      })
+    }
 });
